fa4c859cb8e22400fe72689c8bf00edab7e031bd
Kirtan Khichi <kirtankk12345@gmail.com>
2024-03-25 13:06:43 +0530

    HOme Page updated(routs added to overall ranking

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import overallRankingIcon from '../assets/Overall_WL.png'; // Import the image file
 
 const Home = () => {
   return (
@@ -11,6 +13,13 @@ const Home = () => {
       <p>
         Navigate to the <strong>Rankings</strong> page to view the rankings.
       </p>
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <Link to="/overallRankings">
+          <img src={overallRankingIcon} alt="Overall Rankings" style={{ width: '80px', height: '80px', borderRadius: '50%', border: '2px solid #000', padding: '5px', marginBottom: '10px' }} />
+        </Link>
+        <br />
+        <Link to="/overallRankings">Overall Rankings</Link>
+      </div>
     </div>
   );
 };
