345d39ee4e49dbc690ec88a6aa905632da6e9bb5
Kirtan Khichi <kirtankk12345@gmail.com>
2024-04-16 18:20:13 +0530

    Google analytics added to the page, pushed to check analytics

--- src/App.js
+++ src/App.js
@@ -26,15 +26,15 @@ const App = () => {
   return (
     <Router>
       <div className={darkMode ? 'app-container dark-mode' : 'app-container'}>
-        <div className="app-header">
+        <div className="app-header" style={ {maxHeight:"20vh"}}>
           <img src={logo} alt="Logo" className="app-logo" style={ {width: '100px'}} />
           {/* <h1 className="app-title">U-rank</h1> */}
           {/* <button onClick={toggleDarkMode}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button> */}
 
-          <button onClick={toggleDarkMode} className='toggleButton' style={{ top: "18%", right:"auto", position: 'absolute', zIndex: 999, background:'None', border: 'None'}}>{darkMode ? (
-          <i className="fas fa-sun" style={{ fontSize: '24px' }}></i> // Light mode icon
+          <button onClick={toggleDarkMode} className='toggleButton' style={{ top: "18vh", right:"auto", position: 'absolute', zIndex: 999, borderRadius: "0%"}}>{darkMode ? (
+          <i className="fas fa-sun" style={{ fontSize: '24px' }}> Light</i> // Light mode icon
         ) : (
-          <i className="fas fa-moon" style={{ fontSize: '20px' }}></i> // Dark mode icon
+          <i className="fas fa-moon" style={{ fontSize: '20px' }}> Dark</i> // Dark mode icon
         )}
         </button>
         </div>
